0ddd5c635edc5f1c303180e7263faca32aaab530
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2022-01-12 12:49:38 +0900

    add post create and destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,39 @@
 class PostsController < ApplicationController
+  before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user, only: %i[edit update destroy]
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'post created.'
+      redirect_to root_url
+    else
+      render 'users/index'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = 'Post deleted'
+    redirect_to root_url
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 
   def index
     @posts = current_user.posts if logged_in?
+    @post = Post.new
   end
 
   def new
@@ -39,14 +40,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = 'Please log in.'
-      redirect_to login_url
-    end
   end
 
   def correct_user
